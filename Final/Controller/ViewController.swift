eac0d2a449f98a690d9742067ea7cc194bd0b204
bakenova <arailym.bakenovaa@gmail.com>
2022-05-21 07:47:43 +0600

    first step

---
+++ Final/Controller/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Final
+//
+//  Created by Arailym on 21.05.2022.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+ 
+class ViewController: UIViewController {
+    
+    var player: AVPlayer?
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+        loadVideo()
+    }
+ 
+    private func loadVideo() {
+ 
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.ambient)
+        } catch { }
+ 
+        let path = Bundle.main.path(forResource: "video5206550645464635064", ofType:"mp4")
+ 
+        player = AVPlayer(url: NSURL(fileURLWithPath: path!) as URL)
+        let playerLayer = AVPlayerLayer(player: player)
+ 
+        playerLayer.frame = self.view.frame
+        playerLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        playerLayer.zPosition = -1
+ 
+        self.view.layer.addSublayer(playerLayer)
+ 
+        player?.seek(to: CMTime.zero)
+        player?.play()
+    }
+ 
+}
---
+++ Final/views/DishDetailViewController.swift
@@ -0,0 +1,43 @@
+//
+//  DishDetailViewController.swift
+//  Final
+//
+//  Created by Arailym on 21.05.2022.
+//
+
+import UIKit
+ 
+class DishDetailViewController: UIViewController {
+ 
+    @IBOutlet weak var dishImageView: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var caloriesLbl: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var dplaceOrder: UIButton!
+    
+    var dish: Dish! 
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        populateView()
+        // Do any additional setup after loading the view.
+    }
+    
+    private func populateView(){
+        dishImageView.image = dish.image
+        titleLbl.text = dish.name
+        descriptionLbl.text = dish.description
+        caloriesLbl.text = dish.formattedCalories
+    }
+    
+    @IBAction func BtnClicked(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "New order", message: "Your order is accepted!", preferredStyle: .alert)
+        let Ok = UIAlertAction(title: "Ok", style: .cancel)
+        alert.addAction(Ok)
+        present(alert, animated: true, completion: nil)
+    }
+    
+ 
+}
+ 
