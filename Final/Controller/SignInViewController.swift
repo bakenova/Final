bb1b1f2da4fd4c2f3295884600c411c4256a29e8
Yerkezhan <erkezhanryskhanova@gmail.com>
2022-05-21 17:00:16 -0700

    6.00, 22/05

--- Final/Controller/SignInViewController.swift
+++ Final/Controller/SignInViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignInViewController: UIViewController {
 
     @IBOutlet weak var emailTF: TextFieldPadding!
     @IBOutlet weak var passwordTF: TextFieldPadding!
     @IBOutlet weak var warningLbl: UILabel!
+    
+    @IBOutlet weak var constraintMove: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,27 +22,49 @@ class SignInViewController: UIViewController {
     }
     
     @IBAction func backArrowPressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+        let tableVC = self.storyboard?.instantiateViewController(withIdentifier: "SignUpViewController") as! SignUpViewController
+        tableVC.modalPresentationStyle = .fullScreen
+        self.present(tableVC, animated: true, completion: nil)
     }
     
     @IBAction func signUpPressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+        let tableVC = self.storyboard?.instantiateViewController(withIdentifier: "SignUpViewController") as! SignUpViewController
+        tableVC.modalPresentationStyle = .fullScreen
+        self.present(tableVC, animated: true, completion: nil)
     }
     
-    @IBAction func logInPressed(_ sender: Any) {
+        
+    func showCreateAccount(email: String, passw: String){
+        warningLbl.isHidden = false
+        constraintMove.constant = 30
+//        let alert = UIAlertController(title: "The account does not exist", message: "", preferredStyle: .alert)
+//        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler:{_ in}))
+//
+//        present(alert, animated: true)
     }
     
-    
-    
-    //
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logInPressed(_ sender: UIButton) {
+        guard let email = emailTF.text, !email.isEmpty,
+              let password = passwordTF.text, !password.isEmpty else {
+            let alert = UIAlertController(title: "The field is empty", message: "Please, fill the field", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler:{_ in}))
+            present(alert, animated: true)
+            print("The field is empty!")
+            return
+        }
+        
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: { result, error in
+            
+            guard error == nil else {
+                self.showCreateAccount(email: email, passw: password)
+                return
+            }
+            print("User has signed in")
+            let tableVC = self.storyboard?.instantiateViewController(withIdentifier: "UITabBarController") as! UITabBarController
+            tableVC.modalPresentationStyle = .fullScreen
+            self.present(tableVC, animated: true, completion: nil)
+        })
     }
-    */
+    
 
 }
